40d614214920b2eedd8029ee7cedb9e967930652
Mavis Ou <mmmavisss@gmail.com>
2014-08-28 13:44:32 -0700

    Fix broken link + styling tweaks

--- themes/OneMozilla-PolicyFellows/footer.php
+++ themes/OneMozilla-PolicyFellows/footer.php
@@ -28,7 +28,7 @@
         <li><a href="https://www.mozilla.org/en-US/about/legal.html" rel="external"><?php _e('Legal Notices', 'onemozilla'); ?></a></li>
         <li><a href="https://www.mozilla.org/en-US/legal/fraud-report/index.html" rel="external"><?php _e('Report Trademark Abuse', 'onemozilla'); ?></a></li>
         <li><a href="https://github.com/mozilla/advocacy.mozilla.org" rel="external"><?php _e('Theme Code ', 'onemozilla'); ?></a></li>
-        <li><a href="" rel="external" class="red">Donate [FIXME,need link]</a></li>
+        <li><a href="https://sendto.mozilla.org/page/contribute/Give-Now?&source=mofoadvocacy_site" rel="external" class="red">Donate</a></li>
       </ul>
     </nav>
   </div>
