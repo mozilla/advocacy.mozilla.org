72fabb8cff0b01bcca54322a41e5e7b018fa483f
Mavis Ou <mmmavisss@gmail.com>
2014-09-25 15:23:56 -0400

    Take off 'Coming Soon' version

--- themes/OneMozilla-PolicyFellows/404.php
+++ themes/OneMozilla-PolicyFellows/404.php
@@ -1,184 +1,19 @@
-<!DOCTYPE html>
-<html lang="en-US" id="mozilla-blog">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- For sharing & search engines -->
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content="https://advocacy.mozilla.org/wp-content/uploads/2014/09/logo-og.png" />
-  <meta property="og:image:width" content="1400" />
-  <meta property="og:image:height" content="1400" />
-  <meta property="og:site_name" content="Mozilla Advocacy">
-  <meta property="og:determiner" content="" />
-  <meta property="og:locale" content="en_US" />
-  <meta property="og:title" content="Mozilla's Policy & Advocacy Program - Coming Soon">
-  <meta property="og:url" content="http://localhost:8888/">
-  <meta property="description" content="We are building a global movement to protect the free and open Web." />
-  <meta property="og:description" content="We are building a global movement to protect the free and open Web.">
-  <!-- <meta property="og:description" content=" "> -->
-  <link rel="canonical" href="https://advocacy.mozilla.org" />
-  <!-- /sharing -->
+<?php get_header(); ?>
 
+<div id="content-main" class="main" role="main">
 
-  <meta name="title" content="Coming Soon | Mozilla Advocacy">
-  <meta name="description" content=" ">
+  <article class="page not-found">
+    <header class="entry-header">
+      <h1 class="entry-title"><?php _e( 'Well, this is embarrassing…', 'onemozilla' ); ?></h1>
+    </header>
+    <div class="entry-content">
+      <h2><?php _e( 'We hate to say it, but we couldn’t find the page or file you’re looking for.', 'onemozilla' ); ?></h2>
+      <p><?php _e( 'If you typed in the address, try double-checking the spelling.', 'onemozilla' ); ?></p>
+      <p><?php _e( 'If you followed a link from somewhere, please let us know at <em>webmaster at mozilla dot com</em>. Be sure to tell us where you came from and what you were looking for, and we’ll do our best to fix it.', 'onemozilla' ); ?></p>
+    </div>
+  </article>
 
-  <meta name="Rating" content="General">
-  <!--[if IE]>
-  <meta name="MSSmartTagsPreventParsing" content="true">
-  <meta http-equiv="imagetoolbar" content="no">
-  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <script src="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/js/html5.js"></script>
-  <![endif]-->
+</div><!-- #content-main -->
 
-  <link rel="copyright" href="#colophon">
-  <link rel="profile" href="http://gmpg.org/xfn/11">
-  <link rel="shortcut icon" type="image/ico" href="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/img/favicon.ico">
-  <link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=3.9.2' type='text/css' media='all' />
-<link rel='stylesheet' id='dashicons-css'  href='http://localhost:8888/wp-includes/css/dashicons.min.css?ver=3.9.2' type='text/css' media='all' />
-<link rel='stylesheet' id='admin-bar-css'  href='http://localhost:8888/wp-includes/css/admin-bar.min.css?ver=3.9.2' type='text/css' media='all' />
-<link rel='stylesheet' id='fontawesome-css'  href='http://localhost:8888/wp-content/plugins/olevmedia-shortcodes/assets/css/font-awesome.min.css?ver=3.9.2' type='text/css' media='all' />
-<link rel='stylesheet' id='omsc-shortcodes-css'  href='http://localhost:8888/wp-content/plugins/olevmedia-shortcodes/assets/css/shortcodes.css?ver=3.9.2' type='text/css' media='all' />
-<link rel='stylesheet' id='omsc-shortcodes-tablet-css'  href='http://localhost:8888/wp-content/plugins/olevmedia-shortcodes/assets/css/shortcodes-tablet.css?ver=3.9.2' type='text/css' media='screen and (min-width: 768px) and (max-width: 959px)' />
-<link rel='stylesheet' id='omsc-shortcodes-mobile-css'  href='http://localhost:8888/wp-content/plugins/olevmedia-shortcodes/assets/css/shortcodes-mobile.css?ver=3.9.2' type='text/css' media='screen and (max-width: 767px)' />
-<link rel='stylesheet' id='sand-css'  href='http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/colors/sand/sand.css' type='text/css' media='all' />
-<script type='text/javascript' src='http://localhost:8888/wp-includes/js/jquery/jquery.js?ver=1.11.0'></script>
-<script type='text/javascript' src='http://localhost:8888/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
-<script type='text/javascript' src='http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/js/fc-checkcomment.js?ver=3.9.2'></script>
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost:8888/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost:8888/wp-includes/wlwmanifest.xml" />
-<link rel='canonical' href='http://localhost:8888/' />
-<link rel='shortlink' href='http://localhost:8888/' />
-<style type="text/css" media="print">#wpadminbar { display:none; }</style>
-<style type="text/css" media="screen">
-  html { margin-top: 32px !important; }
-  * html body { margin-top: 32px !important; }
-  @media screen and ( max-width: 782px ) {
-    html { margin-top: 46px !important; }
-    * html body { margin-top: 46px !important; }
-  }
-</style>
-    <link rel="stylesheet" type="text/css" media="screen,projection" href="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/style.css">
-  <link rel="stylesheet" href="//code.cdn.mozilla.net/fonts/fira.css">
-  <link rel="stylesheet" type="text/css" media="all" href="https://www.mozilla.org/tabzilla/media/css/tabzilla.css">
-  <link rel="stylesheet" type="text/css" media="print" href="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/css/print.css">
-  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-  <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="all" href="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/css/ie7.css"><![endif]-->
-  <link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
-  <link rel="alternate" type="application/rss+xml" title="Mozilla Advocacy RSS Feed" href="http://localhost:8888/feed/">
-  <link rel="pingback" href="http://localhost:8888/xmlrpc.php">
-  <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/js/jquery.validate.min.js"></script>
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-49796218-9', 'auto');
-    ga('send', 'pageview');
-
-  </script>
-  <title>Mozilla Advocacy</title>
-
-    <script>
-$(document).ready(function(){
-  $("#navtoggle").click(function(){
-    $("#mobilenav").toggle(500);
-  });
-});
-</script>
-</head>
-
-<body class="home page page-id-320 page-template page-template-page-coming-soon-php logged-in admin-bar no-customize-support sand page-coming-soon">
-<div id="page"><div class="wrap">
-    <main id="content">
-
-    <div id="content-main" class="main coming-soon" role="main">
-      <img src="/wp-content/themes/OneMozilla-PolicyFellows/img/mozilla-advocacy.png" />
-      <div class="white-container">
-        <h1 class="entry-title">Mozilla Advocacy &mdash; Coming Soon</h1>
-      </div>
-
-    </div><!-- #content-main -->
-
-  </main><!-- #content -->
-</div></div><!-- /.wrap /#page -->
-
-
-
-<footer id="site-info" role="contentinfo">
-  <div class="wrap">
-    <p id="foot-logo">
-      <a class="top" href="#page">Return to top</a>
-      <a class="logo" href="http://mozilla.org" rel="external">Mozilla</a>
-    </p>
-
-    <p id="colophon">
-      Except where otherwise <a href="http://www.mozilla.org/en-US/about/legal.html#site" rel="external">noted</a>, content on this site is licensed under the <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="external license">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.    </p>
-
-    <nav id="nav-meta">
-      <ul role="navigation">
-        <li><a href="https://www.mozilla.org/contact/" rel="external">Contact Us</a></li>
-        <li><a href="https://www.mozilla.org/en-US/privacy/" rel="external">Privacy Policy</a></li>
-        <li><a href="https://www.mozilla.org/en-US/about/legal.html" rel="external">Legal Notices</a></li>
-        <li><a href="https://www.mozilla.org/en-US/legal/fraud-report/index.html" rel="external">Report Trademark Abuse</a></li>
-        <li><a href="https://github.com/mozilla/advocacy.mozilla.org" rel="external">Theme Code </a></li>
-        <li><a href="https://sendto.mozilla.org/page/contribute/Give-Now?&source=mofoadvocacy_site" rel="external" class="red">Donate</a></li>
-      </ul>
-    </nav>
-  </div>
-</footer>
-
-<script src="https://www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
-<script src="http://localhost:8888/wp-content/themes/OneMozilla-PolicyFellows/js/ui.js"></script>
-
-<script type="text/javascript">jQuery(function(){omShortcodes.init(["buttons","tooltips","toggle","tabs","responsivebox","counter"]);});</script><script type='text/javascript' src='http://localhost:8888/wp-includes/js/admin-bar.min.js?ver=3.9.2'></script>
-<script type='text/javascript' src='http://localhost:8888/wp-content/plugins/olevmedia-shortcodes/assets/js/shortcodes.js?ver=3.9.2'></script>
-<script type='text/javascript' src='http://localhost:8888/wp-includes/js/comment-reply.min.js?ver=3.9.2'></script>
-  <script type="text/javascript">
-    (function() {
-      var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
-
-      request = true;
-
-      b[c] = b[c].replace( rcs, ' ' );
-      b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
-    }());
-  </script>
-      <div id="wpadminbar" class="nojq nojs" role="navigation">
-      <a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1">Skip to toolbar</a>
-      <div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="Top navigation toolbar." tabindex="0">
-        <ul id="wp-admin-bar-root-default" class="ab-top-menu">
-    <li id="wp-admin-bar-wp-logo" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://localhost:8888/wp-admin/about.php" title="About WordPress"><span class="ab-icon"></span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-wp-logo-default" class="ab-submenu">
-    <li id="wp-admin-bar-about"><a class="ab-item"  href="http://localhost:8888/wp-admin/about.php">About WordPress</a>   </li></ul><ul id="wp-admin-bar-wp-logo-external" class="ab-sub-secondary ab-submenu">
-    <li id="wp-admin-bar-wporg"><a class="ab-item"  href="https://wordpress.org/">WordPress.org</a>   </li>
-    <li id="wp-admin-bar-documentation"><a class="ab-item"  href="http://codex.wordpress.org/">Documentation</a>    </li>
-    <li id="wp-admin-bar-support-forums"><a class="ab-item"  href="https://wordpress.org/support/">Support Forums</a>   </li>
-    <li id="wp-admin-bar-feedback"><a class="ab-item"  href="https://wordpress.org/support/forum/requests-and-feedback">Feedback</a>    </li></ul></div>    </li>
-    <li id="wp-admin-bar-site-name" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://localhost:8888/wp-admin/">Mozilla Advocacy</a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-site-name-default" class="ab-submenu">
-    <li id="wp-admin-bar-dashboard"><a class="ab-item"  href="http://localhost:8888/wp-admin/">Dashboard</a>    </li></ul><ul id="wp-admin-bar-appearance" class="ab-submenu">
-    <li id="wp-admin-bar-themes"><a class="ab-item"  href="http://localhost:8888/wp-admin/themes.php">Themes</a>    </li>
-    <li id="wp-admin-bar-customize" class="hide-if-no-customize"><a class="ab-item"  href="http://localhost:8888/wp-admin/customize.php?url=http%3A%2F%2Flocalhost%3A8888%2F">Customize</a>   </li>
-    <li id="wp-admin-bar-widgets"><a class="ab-item"  href="http://localhost:8888/wp-admin/widgets.php">Widgets</a>   </li>
-    <li id="wp-admin-bar-menus"><a class="ab-item"  href="http://localhost:8888/wp-admin/nav-menus.php">Menus</a>   </li>
-    <li id="wp-admin-bar-header"><a class="ab-item"  href="http://localhost:8888/wp-admin/themes.php?page=custom-header">Header</a>   </li></ul></div>    </li>
-    <li id="wp-admin-bar-updates"><a class="ab-item"  href="http://localhost:8888/wp-admin/update-core.php" title="4 Plugin Updates, Translation Updates"><span class="ab-icon"></span><span class="ab-label">5</span><span class="screen-reader-text">4 Plugin Updates, Translation Updates</span></a>   </li>
-    <li id="wp-admin-bar-comments"><a class="ab-item"  href="http://localhost:8888/wp-admin/edit-comments.php" title="0 comments awaiting moderation"><span class="ab-icon"></span><span id="ab-awaiting-mod" class="ab-label awaiting-mod pending-count count-0">0</span></a>    </li>
-    <li id="wp-admin-bar-new-content" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://localhost:8888/wp-admin/post-new.php" title="Add New"><span class="ab-icon"></span><span class="ab-label">New</span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-new-content-default" class="ab-submenu">
-    <li id="wp-admin-bar-new-post"><a class="ab-item"  href="http://localhost:8888/wp-admin/post-new.php">Post</a>    </li>
-    <li id="wp-admin-bar-new-media"><a class="ab-item"  href="http://localhost:8888/wp-admin/media-new.php">Media</a>   </li>
-    <li id="wp-admin-bar-new-page"><a class="ab-item"  href="http://localhost:8888/wp-admin/post-new.php?post_type=page">Page</a>   </li>
-    <li id="wp-admin-bar-new-user"><a class="ab-item"  href="http://localhost:8888/wp-admin/user-new.php">User</a>    </li></ul></div>    </li>
-    <li id="wp-admin-bar-edit"><a class="ab-item"  href="http://localhost:8888/wp-admin/post.php?post=320&#038;action=edit">Edit Page</a>   </li></ul><ul id="wp-admin-bar-top-secondary" class="ab-top-secondary ab-top-menu">
-    <li id="wp-admin-bar-search" class="admin-bar-search"><div class="ab-item ab-empty-item" tabindex="-1"><form action="http://localhost:8888/" method="get" id="adminbarsearch"><input class="adminbar-input" name="s" id="adminbar-search" type="text" value="" maxlength="150" /><input type="submit" class="adminbar-button" value="Search"/></form></div>   </li>
-    <li id="wp-admin-bar-my-account" class="menupop"><a class="ab-item"  aria-haspopup="true" href="http://localhost:8888/wp-admin/profile.php" title="My Account">Howdy, mavis</a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-user-actions" class="ab-submenu">
-    <li id="wp-admin-bar-user-info"><a class="ab-item" tabindex="-1" href="http://localhost:8888/wp-admin/profile.php"><span class='display-name'>mavis</span></a>    </li>
-    <li id="wp-admin-bar-edit-profile"><a class="ab-item"  href="http://localhost:8888/wp-admin/profile.php">Edit My Profile</a>    </li>
-    <li id="wp-admin-bar-logout"><a class="ab-item"  href="http://localhost:8888/wp-login.php?action=logout&#038;_wpnonce=31c03335cc">Log Out</a>   </li></ul></div>    </li></ul>      </div>
-            <a class="screen-reader-shortcut" href="http://localhost:8888/wp-login.php?action=logout&#038;_wpnonce=31c03335cc">Log Out</a>
-          </div>
-
-
-</body>
-</html>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
