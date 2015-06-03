1e632874376b17371b0f8525db36e6f1c19bd7f5
Jon Buckley <jon@jbuckley.ca>
2015-06-03 17:39:22 -0400

    Revert "Open Fellows"
    This reverts commit 7dbcd1e38e25a2847e94cc0cc7f80c0ce8accc8e.

--- common/scripts/ui.js
+++ common/scripts/ui.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   relocatedSubmenuBar();
+  applicationFormHandler();
 });
 
 function relocatedSubmenuBar() {
@@ -13,3 +14,40 @@ function relocatedSubmenuBar() {
   }
 }
 
+function applicationFormHandler() {
+  var formURL = "https://docs.google.com/a/mozillafoundation.org/forms/d/1qHke2PWGy-vrQ8wb9V2uWVmgh8g0GGJ0PQ1JMOhkvVs/formResponse"; // real form
+  var privacyCheckboxName = "entry.2055255229";
+
+  $("#ss-form").submit(function(event) {
+    event.preventDefault();
+  });
+
+  $("#ss-form").validate({
+    messages: {
+      "entry.2055255229": "Please check this box if you want to proceed."  // privacy policy checkbox
+    },
+    errorPlacement: function(error, element) {
+      if (element.attr("name") == privacyCheckboxName ) { // privacy policy checkbox
+        error.text("Please check this box if you want to proceed.");
+        error.insertAfter(element.siblings(".ss-choice-label"));
+      } else {
+        error.hide(); // highlight other missing fields with red border instead showing error messages
+      }
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        url: formURL,
+        data: $(form).serialize(),
+        type: "POST",
+        crossDomain: true,
+        dataType: "json",
+        complete: function(jqXHR, textStatus) {
+          // redirect to thank you page if Google has successfully received the data
+          if ( jqXHR.status == "0" || jqXHR.status == "200" ) {
+            window.location.href = "/thank-you";
+          }
+        }
+      });
+    }
+  });
+}
