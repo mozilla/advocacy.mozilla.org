c1e3e36db87dc08e16536d22f2577580100d6349
Ali Al Dallal <ali@alicoding.com>
2015-06-08 11:58:05 -0400

    Fix date for feeds

--- common/scripts/feed.js
+++ common/scripts/feed.js
@@ -6,7 +6,8 @@ feed.load(function (data) {
     // Parse data depending on the specified response format, default is JSON.
     $.each(data.feed.entries, function(index, item) {
       var d = new Date(item['publishedDate']);
-      var date = monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getYear();
+      // getYear() will return 115 for 2015, so we need to - by 100 to get 15
+      var date = monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + (d.getYear() - 100);
       $("ul#rss-feed").append('<li><a class="rsswidget" href="' + item['link'] + '">'+item['title']+'</a> <span class="rss-date">'+ date +'</span></li>');
     });
 });
