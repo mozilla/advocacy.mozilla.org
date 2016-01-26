a3953494f836e43baa1a156cfe0a41def650264d
ScottDowne <scott.downe@gmail.com>
2016-01-26 14:49:58 -0500

    stubbing in the rest of overview

--- pages/open-web-fellows/overview.jsx
+++ pages/open-web-fellows/overview.jsx
@@ -24,6 +24,22 @@ var Icon = React.createClass({
   }
 });
 
+var PlanetContent = React.createClass({
+  render: function() {
+    return (
+      <div>Planet Image</div>
+    );
+  }
+});
+
+var Organization = React.createClass({
+  render: function() {
+    return (
+      <div>Organization</div>
+    );
+  }
+});
+
 module.exports = React.createClass({
   render: function() {
     return (
@@ -45,9 +61,66 @@ module.exports = React.createClass({
             </Icon>
           </IconContainer>
         </div>
-        <div className="content-container">
-
+        <div className="content-container grey">
+          <h2>Who can become a Fellow?</h2>
+          <div className="half">
+            <p>It could be you! Broadly speaking, we are looking for makers – those who see a problem in the world and can solve it through technology or media. The right candidate may bring specialities in design, development, storytelling, research and policy analysis, and should be comfortable performing as a technologist, a tinkerer, and a curious contributor to the fellowship program. <b>You do not need to have previous experience with Internet advocacy, policy or activism.</b> A lot of campaign design, community organizing, education, and</p>
+          </div>
+          <div className="half">
+            <p>coalition building happen through teamwork and skills like data visualization and participatory learning. You can be at any stage in your career, provided you are open to learning.</p>
+            <p>We are an international program with host organizations around the world; fellows will be placed in Canada, Chile, Kenya, the European Union, and the United States. We assist fellows with visa applications for working and traveling during the fellowship.</p>
+          </div>
         </div>
+        <PlanetContent/>
+        <div className="content-container">
+          <h2>Host Organizations</h2>
+          <p>Each year, fellows spend 10 months embedded at leading policy & advocacy organizations dedicated to safeguarding the open Internet as a global public resource. Fellows are based in the town of their host organization unless special circumstances dictate otherwise. Read the <a href="#">blog post</a> to find out what host organizations are looking for. The 2016 host organizations are:</p>
+        </div>
+        <div className="organization-row">
+          <Organization
+            imgSrc=""
+            linkTitle="Nairobi, Kenya"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="Toronto, Canada"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="New York, NY"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="New York, NY"
+            linkLoation=""
+          />
+        </div>
+        <div className="organization-row">
+          <Organization
+            imgSrc=""
+            linkTitle="Santiago, Chile"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="Brussels, Belgium"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="San Francisco, CA"
+            linkLoation=""
+          />
+          <Organization
+            imgSrc=""
+            linkTitle="London, UK"
+            linkLoation=""
+          />
+        </div>
+        <HeroUnit></HeroUnit>
         <Footer/>
       </div>
     );
