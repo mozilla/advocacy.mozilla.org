fca75efc1f8007c2c563a02e2c69f7514f603019
ScottDowne <scott.downe@gmail.com>
2016-01-26 13:07:07 -0500

    starting site updates

--- components/hero-unit.jsx
+++ components/hero-unit.jsx
@@ -1,6 +1,42 @@
 var React = require('react');
+
 module.exports = React.createClass({
+  calculateDensity: function () {
+    var
+        ratio;
+
+    if (typeof window !== 'undefined' && window.devicePixelRatio > 1.5) {
+      ratio = 2;
+    } else {
+      ratio = 1;
+    }
+
+    return ratio;
+  },
+
+  getInitialState: function () {
+    var image = this.props.image;
+    if (this.calculateDensity() === 2) {
+      image = this.props.image2x || this.props.image;
+    }
+    return {
+      image: image
+    };
+  },
+
+  propTypes: {
+    'image': React.PropTypes.string.isRequired
+  },
+
   render: function() {
-    return (<div>hero-unit</div>);
+    return (
+      <div className="hero-unit-container">
+        <div className="hero-unit" style={{
+          backgroundImage: 'url(' + this.state.image + ')'
+        }}>
+          {this.props.children}
+        </div>
+      </div>
+    );
   }
 });
