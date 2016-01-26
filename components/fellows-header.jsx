64f6072033f24555d5fb3f9598f27d0c9e9e9116
ScottDowne <scott.downe@gmail.com>
2016-01-26 13:07:07 -0500

    starting site updates

--- components/fellows-header.jsx
+++ components/fellows-header.jsx
@@ -1,6 +1,23 @@
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+
 module.exports = React.createClass({
   render: function() {
-    return (<div>fellows-header</div>);
+    return (
+      <div className="fellows-header">
+        <div className="nav-items">
+          <div className="nav-link-container">
+            <Link to="open-web-fellows-overview">Overview</Link>
+          </div>
+          <div className="nav-link-container">
+            <Link to="open-web-fellows-info">Info</Link>
+          </div>
+          <div className="nav-link-container">
+            <Link to="open-web-fellows-fellows">Fellows</Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,33 @@
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var ImageTag = require('./imagetag.jsx');
+
 module.exports = React.createClass({
   render: function() {
-    return (<div>header</div>);
+    var logoImage = this.props.logoImage || "wedonthavethisyet.gif";
+    return (
+      <div className="header">
+        <div className="header-content">
+          <div className="nav-home">
+            <Link to="home">
+              <ImageTag src1x={logoImage}
+                alt="advocacy logo"/>
+            </Link>
+          </div>
+          <div className="nav-items">
+            <div className="nav-link-container">
+              <Link to="home">Home</Link>
+            </div>
+            <div className="nav-link-container">
+              <Link to="open-web-fellows-overview">Open Web Fellows</Link>
+            </div>
+            <div className="nav-link-container">
+              <a href="">Blog</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 });
