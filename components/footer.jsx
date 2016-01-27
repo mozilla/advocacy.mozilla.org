e2417e47adb91371448faa5a543be87bef89792e
ScottDowne <scott.downe@gmail.com>
2016-01-26 21:12:36 -0500

    finished footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ var Icon = React.createClass({
     return (
       <div className="icon-container">
         <div className="icon">
-          <div className="icon-circle"></div>
+          <img className="footer-icon" src={this.props.src}></img>
           <a href={this.props.href}>{this.props.children}</a>
         </div>
       </div>
@@ -15,15 +15,19 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="footer">
-        <Icon href="" src="" title="">Contact Us</Icon>
-        <Icon href="" src="" title="">Connext on Twitter</Icon>
-        <Icon href="" src="" title="">Legal</Icon>
-        <Icon href="" src="" title="">Privacy Policy</Icon>
-        <div>mozilla</div>
-        <div className="horizontal-rule"></div>
-        <p>
-          Mozilla is a global non-profit dedicated to putting you in control of your online experience and shaping the future of the web for the public good.
-        </p>
+        <Icon href="" src="/assets/footer-icon-help.svg" title="">Contact Us</Icon>
+        <Icon href="" src="/assets/footer-icon-twitter.svg" title="">Connext on Twitter</Icon>
+        <Icon href="" src="/assets/footer-icon-terms.svg" title="">Legal</Icon>
+        <Icon href="" src="/assets/footer-icon-privacy.svg" title="">Privacy Policy</Icon>
+        <div className="footer-content">
+          <div>
+            <img height="30" width="105" src="/assets/mozilla-wordmark.svg"/>
+          </div>
+          <div className="horizontal-rule"></div>
+          <p>
+            Mozilla is a global non-profit dedicated to putting you in control of your online experience and shaping the future of the web for the public good. Visit us at <a href="//mozilla.org">mozilla.org</a>
+          </p>
+        </div>
       </div>
     );
   }
