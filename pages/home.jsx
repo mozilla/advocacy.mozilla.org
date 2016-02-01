c4c1c8772e8fe1936ee5ef5b90c895ddc3c76ae4
ScottDowne <scott.downe@gmail.com>
2016-02-01 11:58:02 -0500

    remove a warning

--- pages/home.jsx
+++ pages/home.jsx
@@ -36,6 +36,7 @@ module.exports = React.createClass({
         <div className="page">
           <ContentContainer className="grey center-align">
             <HomePageItem
+              alt="net neutrality image"
               src1x="/assets/home-neutrality.jpg"
               src2x="/assets/home-neutrality@2x.jpg"
               label="Net Neutrality"
@@ -45,6 +46,7 @@ module.exports = React.createClass({
               In 2015, we fought to keep the Internet free of fast lanes, slow lanes and corporate control. Our community petitioned Congress and made a difference.
             </HomePageItem>
             <HomePageItem
+              alt="open web fellows image"
               src1x="/assets/home-fellows.jpg"
               src2x="/assets/home-fellows@2x.jpg"
               label="Open Web Fellows"
@@ -54,6 +56,7 @@ module.exports = React.createClass({
               The Open Web Fellows program places the best emerging technology talent at civil society organizations around the globe. As threats to digital freedom proliferate, it's critical to have capable leaders.
             </HomePageItem>
             <HomePageItem
+              alt="usa freedom act image"
               src1x="/assets/home-freedom.jpg"
               src2x="/assets/home-freedom@2x.jpg"
               label="USA FREEDOM Act"
@@ -63,6 +66,7 @@ module.exports = React.createClass({
               Mass Surveillance is a threat to privacy and trust online. Our community fought for the successful passage of the USA FREEDOM Act, a first step toward reforming mass surveillance.
             </HomePageItem>
             <HomePageItem
+              alt="net policy blog image"
               src1x="/assets/home-policy.jpg"
               src2x="/assets/home-policy@2x.jpg"
               label="Net Policy Blog"
