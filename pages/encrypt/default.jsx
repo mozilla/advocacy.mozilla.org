c3d8ca42860a8af7b4587e1b4ec8ab878adc6b0b
Ali Al Dallal <ali@alicoding.com>
2016-02-02 16:22:14 -0500

    more

--- pages/encrypt/default.jsx
+++ pages/encrypt/default.jsx
@@ -2,8 +2,9 @@ var React = require('react');
 var Footer = require('../../components/footer.jsx');
 var HeroUnit = require('../../components/hero-unit.jsx');
 var Video = require('../../components/video.jsx');
-var ContentContainer = require('../../components/content-container.jsx');
+var ShareThisNow = require('../../components/share-this-now');
 var ImageTag = require('../../components/imagetag.jsx');
+var Icon = require('../../components/footer-icon.jsx');
 
 module.exports = React.createClass({
   render: function() {
@@ -13,24 +14,19 @@ module.exports = React.createClass({
           <div className="header-content">
             <ImageTag className="logo" height={30} width={105} src1x="/assets/logo-mozilla.svg" alt="Mozilla logo"/>
           </div>
-          </div>
+        </div>
         <HeroUnit>
           <div className="encryptText">
-            <h1>hehe</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <img src="/assets/home-freedom.jpg"/>
           </div>
         </HeroUnit>
         <div className="page">
           <Video className="videoSection"/>
-          <ContentContainer className="encryptMeta">
-            <h2>
-              Why does encryption matter?
-            </h2>
-            <span className="videoMeta">FEBUARY 05, 2016  |  EPISODE 1  | 0:53</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </ContentContainer>
+          <ShareThisNow/>
         </div>
-        <Footer/>
+        <Footer>
+          <Icon href="https://twitter.com/MozillaAdvocacy" src="/assets/footer-icon-twitter.svg" title="">FIXME</Icon>
+        </Footer>
       </div>
     );
   }
