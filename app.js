ad1b2d985b14ebc7e1410230272e60bcbd2e744e
Ali Al Dallal <ali@alicoding.com>
2016-02-03 17:41:22 -0500

    Add server side route handler

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   Habitat = require('habitat'),
   path = require('path'),
-  compression = require('compression');
+  compression = require('compression'),
+  ReactRouting = require('./lib/react-server-route.js');
 
 Habitat.load();
 
@@ -14,14 +15,14 @@ app.use(function(err, req, res, next) {
   res.send(err);
 });
 
-app.get('/encrypt/*', function (request, response) {
-  response.sendFile(path.join(__dirname, '/public/encrypt/index.html'));
+app.get('/encrypt*', function(request, response) {
+  ReactRouting(request, response);
 });
 
-app.get('*', function (request, response) {
+app.get('*', function(request, response) {
   response.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(env.get('PORT'), function () {
+app.listen(env.get('PORT'), function() {
   console.log('Server listening ( http://localhost:%d )', env.get('PORT'));
 });
