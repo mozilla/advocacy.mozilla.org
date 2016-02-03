f83d476e813fbd4c157f0521b7db44bb689518e5
Alan Mooiman <amooiman@gmail.com>
2016-02-03 13:55:06 -0800

    General layout in place

--- pages/encrypt/v2.jsx
+++ pages/encrypt/v2.jsx
@@ -10,19 +10,19 @@ var ShareThisNow = require('../../components/share-this-now');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div className="encrypt">
+      <div className="encrypt v2">
         <header className="header">
           <div className="header-content">
             <ImageTag className="logo" height={30} width={105} src1x="/assets/logo-mozilla.svg" alt="Mozilla logo"/>
           </div>
+          <div className="encryptText">
+            <img src="http://dummyimage.com/960x200/5381b9/f46060.png&text=Encrypt"/>
+          </div>
         </header>
-        <div className="encryptText">
-          <img src="http://dummyimage.com/960x200/5381b9/f46060.png&text=Encrypt"/>
-        </div>
-        <div className="page">
-          <div className="videoSection-v2">
+        <main className="page">
+          <div className="videoSection">
             <Video className="left-section" poster="/assets/home-freedom.jpg">
-              <div className="encrypt-meta-v2">
+              <div className="encrypt-meta">
                 <span className="videoMeta"><span className="white-text">EPISODE 1</span> &nbsp;  <span className="purple-text">FEBUARY 08, 2016 &nbsp; 0:53</span></span>
                 <span className="videoTitle">
                   Privacy Lets You Be You
@@ -30,22 +30,29 @@ module.exports = React.createClass({
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </div>
             </Video>
-            <Signup className="right-section">
-              <CTA
-                HrClassName="v2-cta-hr"
-                headerClassName="v2-cta-header"
-                textClassName="v2-cta-text"
-                header="Join Mozilla"
-                text="For more resources and videos about encryption and other topics essential to protecting the Web, signup for email updates from Mozilla."
-              />
-            </Signup>
           </div>
-          <ShareThisNow/>
-        </div>
+          <div className="dual-cta-wrapper">
+            <div className="signup-wrapper">
+                <CTA
+                  HrClassName="cta-hr signup"
+                  headerClassName="cta-header"
+                  textClassName="cta-text"
+                  header="Join Mozilla"
+                  text="For more resources and videos about encryption and other topics essential to protecting the Web, signup for email updates from Mozilla."
+                />
+                <button className="button button-groove">Sign up</button>
+            </div>
+            <div className="share-wrapper">
+              <ShareThisNow/>
+            </div>
+          </div>
+        </main>
         <Footer>
           <Icon href="https://twitter.com/MozillaAdvocacy" src="/assets/footer-icon-twitter.svg" title="">FIXME</Icon>
         </Footer>
       </div>
+
+
     );
   }
 });
