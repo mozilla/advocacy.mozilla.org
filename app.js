79e23aaf775282f404ebc841148a4274aef5ce32
Ali Al Dallal <ali@alicoding.com>
2016-02-03 11:32:23 -0500

    Multiple build js and css with webpack

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    Habitat = require('habitat'),
-    path = require('path'),
-    compression = require('compression');
+  Habitat = require('habitat'),
+  path = require('path'),
+  compression = require('compression');
 
 Habitat.load();
 
@@ -12,6 +12,10 @@ app.use(compression());
 app.use(express.static(__dirname + '/public', {maxAge: 3600000}));
 app.use(function(err, req, res, next) {
   res.send(err);
+});
+
+app.get('/encrypt/*', function (request, response) {
+  response.sendFile(path.join(__dirname, '/public/encrypt/index.html'));
 });
 
 app.get('*', function (request, response) {
