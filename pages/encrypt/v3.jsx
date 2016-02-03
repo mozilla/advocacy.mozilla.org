701e71708d7d6a99078cc5c322c8c86e826fd2ae
Alan Mooiman <amooiman@gmail.com>
2016-02-02 16:02:08 -0800

    Rename files to match comps

--- pages/encrypt/v3.jsx
+++ pages/encrypt/v3.jsx
@@ -1,14 +1,53 @@
 var React = require('react');
 var Footer = require('../../components/footer.jsx');
-var Header = require('../../components/header.jsx');
+var HeroUnit = require('../../components/hero-unit.jsx');
+var Video = require('../../components/video.jsx');
+var ImageTag = require('../../components/imagetag.jsx');
+var Signup = require('../../components/signup.jsx');
+var CTA = require('../../components/cta.jsx');
+var Icon = require('../../components/footer-icon.jsx');
+var ShareThisNow = require('../../components/share-this-now');
 
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>
-        <Header></Header>
-        V3
-        <Footer></Footer>
+      <div className="encrypt">
+        <div className="header">
+          <div className="header-content">
+            <ImageTag className="logo" height={30} width={105} src1x="/assets/logo-mozilla.svg" alt="Mozilla logo"/>
+          </div>
+        </div>
+        <HeroUnit>
+          <div className="encryptText">
+            <img src="http://dummyimage.com/960x200/5381b9/f46060.png&text=Encrypt"/>
+          </div>
+        </HeroUnit>
+        <div className="page">
+          <div className="videoSection-v3">
+            <Video className="left-section" poster="/assets/home-freedom.jpg">
+              <div className="encrypt-meta-v3">
+                <span className="videoMeta"><span className="white-text">EPISODE 1</span> &nbsp;  <span className="purple-text">FEBUARY 08, 2016 &nbsp; 0:53</span></span>
+                <span className="videoTitle">
+                  Privacy Lets You Be You
+                </span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </Video>
+            <Signup className="right-section">
+              <CTA
+                HrClassName="v3-cta-hr"
+                headerClassName="v3-cta-header"
+                textClassName="v3-cta-text"
+                header="Join Mozilla"
+                text="For more resources and videos about encryption and other topics essential to protecting the Web, signup for email updates from Mozilla."
+              />
+            </Signup>
+          </div>
+          <ShareThisNow/>
+        </div>
+        <Footer>
+          <Icon href="https://twitter.com/MozillaAdvocacy" src="/assets/footer-icon-twitter.svg" title="">FIXME</Icon>
+        </Footer>
       </div>
     );
   }
