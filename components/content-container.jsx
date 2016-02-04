b0170bb216841e8b79aa8b80799a4f4e929c907f
Ali Al Dallal <ali@alicoding.com>
2016-02-04 18:06:01 -0500

    Add mofo-style eslint

--- components/content-container.jsx
+++ components/content-container.jsx
@@ -1,11 +1,10 @@
 var React = require('react');
-var HeroUnit = require('./hero-unit.jsx');
 
 module.exports = React.createClass({
   render: function() {
     var className = "content-container";
     if (this.props.className) {
-      className += " " + this.props.className
+      className += " " + this.props.className;
     }
     return (
       <div className={className}>
