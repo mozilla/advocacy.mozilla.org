5e8bf753051227c8d2295f8cb6b8809f254d48c5
Ali Al Dallal <ali@alicoding.com>
2016-02-04 10:43:44 -0500

    Make local dev to not cache file when file changes

--- encrypt-main.jsx
+++ encrypt-main.jsx
@@ -1,15 +1,17 @@
+var Path = require('path');
+
 const home = {
   path: '/encrypt',
-  component: require('./pages/encrypt/default.jsx')
+  pathToFile: Path.join(__dirname, './pages/encrypt/default.jsx')
 };
 
 const v2 = {
   path: '/encrypt/v2',
-  component: require('./pages/encrypt/v2.jsx')
+  pathToFile: Path.join(__dirname, './pages/encrypt/v2.jsx')
 };
 const v3 = {
   path: '/encrypt/v3',
-  component: require('./pages/encrypt/v3.jsx')
+  pathToFile: Path.join(__dirname, './pages/encrypt/v3.jsx')
 };
 
 module.exports = {
