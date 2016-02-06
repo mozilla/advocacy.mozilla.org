bb7705db4ff4f2026d30a5e7f9e9e997f3533139
Alan Mooiman <amooiman@gmail.com>
2016-02-05 16:26:27 -0800

    MOAR CSS

--- components/encrypt-header.jsx
+++ components/encrypt-header.jsx
@@ -7,6 +7,7 @@ module.exports = React.createClass({
       <header className="header">
         <div className="header-content">
           <image className="logo" height="30" width="105" src="/assets/logo-mozilla.svg" alt="Mozilla logo"/>
+	  <div className="header-cta">Get Mozilla Advocacy Updates <button className="button">Sign Up</button></div>
 		<h1 className="encryptText">
 		       <img src="/assets/Encrypt_Placeholder_Image.png" width="960" alt="Encrypt"/>
 		</h1>
--- components/encrypt-video.jsx
+++ components/encrypt-video.jsx
@@ -4,16 +4,20 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="video-wrapper">
-	<video poster="/assets/Encrypt_static_image.png" autoBuffer width={this.props.width} height={this.props.height}>
+	<video aria-describedby="videoDescription" aria-labeledby="videoTitle" poster="/assets/encrypt-poster-dark-1920x1080.jpg" autoBuffer width={this.props.width} height={this.props.height}>
           <source id="mp4" src="http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4" type="video/mp4"/>
         </video>
         <div className="encrypt-meta-wrapper">
           <div className="encrypt-meta">
-            <span className="videoMeta"><span className="white-text">EPISODE 1</span> &nbsp;       <span className="purple-text">FEBUARY 08, 2016 &nbsp; 0:53</span></span>
-            <span className="videoTitle">
-              Privacy Lets You Be You
+	    <span className="videoMeta">
+	      <span className="episodeNumber">EPISODE 1</span>
+	      <time className="video-date" dateTime="2016-02-08">FEBUARY 08, 2016</time>
+	      <span className="duration">0:53</span>
             </span>
-            <p className="video-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	    <div id="videoTitle" className="videoTitle">
+	      Privacy Lets You Be You
+	    </div>
+	    <p id="videoDescription" className="video-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
           </div>
         </div>
       </div>
