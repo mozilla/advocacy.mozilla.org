3d8cf94819d5e845ba599ebe3b5404a7143200d7
Ali Al Dallal <ali@alicoding.com>
2016-02-08 10:27:10 -0500

    Added GA tracking code

--- pages/encrypt/index.jsx
+++ pages/encrypt/index.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 module.exports = React.createClass({
   render: function() {
+    var ga = `
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-49796218-9', 'auto');
+      ga('send', 'pageview');
+    `;
     return (
       <html>
         <head>
@@ -25,6 +33,7 @@ module.exports = React.createClass({
           <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Fira+Sans:300,400,600"/>
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
           <title>Mozilla Advocacy</title>
+          <script dangerouslySetInnerHTML={{__html: ga}}></script>
         </head>
         <body>
           <div id="my-app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
