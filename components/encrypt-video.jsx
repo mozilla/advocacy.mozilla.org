6342ef1eea545c39ca4d470a88eac758a54d9139
Alan Mooiman <amooiman@gmail.com>
2016-02-09 12:53:32 -0800

    Video fading

--- components/encrypt-video.jsx
+++ components/encrypt-video.jsx
@@ -1,13 +1,37 @@
 var React = require(`react`);
+var classNames = require('classnames');
 
 module.exports = React.createClass({
+  componentDidMount() {
+    this.refs.video.addEventListener("playing", (e)=>{
+      console.log('video started playing ');
+    });
+  },
+  playVideo: function(){
+    this.refs.video.play();
+    this.props.setPageState({
+      videoDidStart: true
+    });
+  },
   render: function() {
+    var encryptWrapperClass = classNames({
+      'encrypt-meta-wrapper':true,
+      'fadedOut': this.props.videoDidStart && document.body.clientWidth >= 650
+    });
+    var playButtonClass = classNames({
+      'playButton': true,
+      'fadedOut' : this.props.videoDidStart
+    })
+
     return (
       <div className="video-wrapper">
-        <video id="video" aria-describedby="videoDescription" controls aria-labeledby="videoTitle" poster="/assets/encrypt-poster-dark-1920x1080.jpg" autoBuffer width={this.props.width} height={this.props.height}>
-          <source id="mp4" src="https://d24kjznqej0s8a.cloudfront.net/2016/encryption_campaign/moz.final.1.native.mp4" type="video/mp4"/>
-        </video>
-        <div className="encrypt-meta-wrapper">
+	<div className="video-btn-wrapper">
+	  <button onClick={this.playVideo} className={playButtonClass} aria-label="Play video" ref="playButton"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMTIuMTM1IiBoZWlnaHQ9IjExMi4xMzUiIHZpZXdCb3g9IjAgMCAxMTIuMTM1IDExMi4xMzUiPjxnPjxwYXRoIGZpbGw9IiNmZmYiIGQ9Ik01NS44MjMgMTIuNzc4YzIzLjA5OCAwIDQxLjgyMiAxOC43MjUgNDEuODIyIDQxLjgyMlM3OC45MiA5Ni40MjIgNTUuODIzIDk2LjQyMnMtNDEuODItMTguNzI0LTQxLjgyLTQxLjgyIDE4LjcyMy00MS44MjQgNDEuODItNDEuODI0bTAtNS4zMkMyOS44MyA3LjQ1OCA4LjY4IDI4LjYwNiA4LjY4IDU0LjZzMjEuMTQ4IDQ3LjE0NCA0Ny4xNDMgNDcuMTQ0IDQ3LjE0My0yMS4xNDggNDcuMTQzLTQ3LjE0M1M4MS44MTYgNy40NiA1NS44MjMgNy40NnpNNDYuODggNzIuMzc0VjM3LjY5N2wyMy4xNzcgMTcuMzR6Ii8+PC9nPjwvc3ZnPg==" alt="play icon"/></button>
+	  <video aria-describedby="videoDescription" ref="video" controls={this.props.videoDidStart} aria-labeledby="videoTitle" poster="/assets/encrypt-poster-dark-1920x1080.jpg" autoBuffer width={this.props.width} height={this.props.height}>
+	    <source id="mp4" src="https://d24kjznqej0s8a.cloudfront.net/2016/encryption_campaign/moz.final.1.native.mp4" type="video/mp4"/>
+	  </video>
+	</div>
+	<div className={encryptWrapperClass} ref="metaWrapper">
           <div className="encrypt-meta">
             <span className="videoMeta">
             <span className="episodeNumber">EPISODE 1</span>
