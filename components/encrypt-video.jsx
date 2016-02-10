48b8db69d0b2b002afd508ba01056c9be2ad92cf
Alan Mooiman <amooiman@gmail.com>
2016-02-10 11:10:19 -0800

    Fix encrypt tex overlap

--- components/encrypt-video.jsx
+++ components/encrypt-video.jsx
@@ -56,22 +56,27 @@ module.exports = React.createClass({
       'visible': this.props.videoDidStart && this.props.version !== "3" && !this.props.videoDidEnd && !this.props.videoIsPaused
     });
     var videoClass = classNames({
-      'fadedIn': this.props.videoDidStart
+      'fadedIn': this.props.videoDidStart,
+      'fadedOut': !this.props.videoDidStart
+    });
+    var videoWrapperClass = classNames({
+      'video-wrapper': true,
+      'playing': this.props.videoDidStart && !this.props.videoIsPaused && !this.props.videoDidEnd
     });
 
     return (
-      <div className="video-wrapper">
-	<div ref="theatre" className={videoTheatreClass}></div>
-	<div className="video-btn-wrapper">
-	  <button onClick={this.playVideo} className={playButtonClass} aria-label="Play video" ref="playButton"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMTIuMTM1IiBoZWlnaHQ9IjExMi4xMzUiIHZpZXdCb3g9IjAgMCAxMTIuMTM1IDExMi4xMzUiPjxnPjxwYXRoIGZpbGw9IiNmZmYiIGQ9Ik01NS44MjMgMTIuNzc4YzIzLjA5OCAwIDQxLjgyMiAxOC43MjUgNDEuODIyIDQxLjgyMlM3OC45MiA5Ni40MjIgNTUuODIzIDk2LjQyMnMtNDEuODItMTguNzI0LTQxLjgyLTQxLjgyIDE4LjcyMy00MS44MjQgNDEuODItNDEuODI0bTAtNS4zMkMyOS44MyA3LjQ1OCA4LjY4IDI4LjYwNiA4LjY4IDU0LjZzMjEuMTQ4IDQ3LjE0NCA0Ny4xNDMgNDcuMTQ0IDQ3LjE0My0yMS4xNDggNDcuMTQzLTQ3LjE0M1M4MS44MTYgNy40NiA1NS44MjMgNy40NnpNNDYuODggNzIuMzc0VjM3LjY5N2wyMy4xNzcgMTcuMzR6Ii8+PC9nPjwvc3ZnPg==" alt="play icon"/></button>
-    <img src="/assets/encrypt-poster-dark-1920x1080.jpg" hidden={this.props.videoDidStart} className="video-poster" style={{position:"absolute"}}/>
-	  <video className={videoClass} aria-describedby="videoDescription" ref="video" controls={this.props.videoDidStart} aria-labeledby="videoTitle" poster="/assets/encrypt-poster-dark-1920x1080.jpg" autoBuffer width={this.props.width} height={this.props.height} style={{zIndex:1000}}>
-      A video about why encryption is important. Privacy lets you be you.
-      <source id="mp4" src={videoToPlay.mp4[this.props.version]} type="video/mp4"/>
-      <source id="webm" src={videoToPlay.webm[this.props.version]} type="video/webm"/>
-    </video>
-  </div>
-      <div className={encryptWrapperClass} ref="metaWrapper">
+      <div className={videoWrapperClass}>
+        <div ref="theatre" className={videoTheatreClass}></div>
+          <div className="video-btn-wrapper">
+            <button onClick={this.playVideo} className={playButtonClass} aria-label="Play video" ref="playButton"><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMTIuMTM1IiBoZWlnaHQ9IjExMi4xMzUiIHZpZXdCb3g9IjAgMCAxMTIuMTM1IDExMi4xMzUiPjxnPjxwYXRoIGZpbGw9IiNmZmYiIGQ9Ik01NS44MjMgMTIuNzc4YzIzLjA5OCAwIDQxLjgyMiAxOC43MjUgNDEuODIyIDQxLjgyMlM3OC45MiA5Ni40MjIgNTUuODIzIDk2LjQyMnMtNDEuODItMTguNzI0LTQxLjgyLTQxLjgyIDE4LjcyMy00MS44MjQgNDEuODItNDEuODI0bTAtNS4zMkMyOS44MyA3LjQ1OCA4LjY4IDI4LjYwNiA4LjY4IDU0LjZzMjEuMTQ4IDQ3LjE0NCA0Ny4xNDMgNDcuMTQ0IDQ3LjE0My0yMS4xNDggNDcuMTQzLTQ3LjE0M1M4MS44MTYgNy40NiA1NS44MjMgNy40NnpNNDYuODggNzIuMzc0VjM3LjY5N2wyMy4xNzcgMTcuMzR6Ii8+PC9nPjwvc3ZnPg==" alt="play icon"/></button>
+            <img src="/assets/encrypt-poster-dark-1920x1080.jpg" hidden={this.props.videoDidStart} className="video-poster" style={{position:"absolute"}}/>
+            <video className={videoClass} aria-describedby="videoDescription" ref="video" controls={this.props.videoDidStart} aria-labeledby="videoTitle" poster="/assets/encrypt-poster-dark-1920x1080.jpg" autoBuffer width={this.props.width} height={this.props.height} style={{zIndex:1000}}>
+              A video about why encryption is important. Privacy lets you be you.
+              <source id="mp4" src={videoToPlay.mp4[this.props.version]} type="video/mp4"/>
+              <source id="webm" src={videoToPlay.webm[this.props.version]} type="video/webm"/>
+            </video>
+          </div>
+        <div className={encryptWrapperClass} ref="metaWrapper">
           <div className="encrypt-meta">
             <span className="videoMeta">
             <span className="episodeNumber">EPISODE 1</span>
