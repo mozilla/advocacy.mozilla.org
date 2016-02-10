ae9792d9c5ecb6f2a0e8114eb21b4a97f64eacca
Ali Al Dallal <ali@alicoding.com>
2016-02-09 19:06:32 -0500

    Update social share

--- encrypt-main.jsx
+++ encrypt-main.jsx
@@ -6,7 +6,7 @@ import { Router, Route, browserHistory } from 'react-router';
 export default (
   (<Router history={browserHistory}>
     <Route path="/encrypt" component={require(`./pages/encrypt/default.jsx`)}/>
-    <Route path="/encrypt/v2" component={require(`./pages/encrypt/v2.jsx`)}/>
-    <Route path="/encrypt/v3" component={require(`./pages/encrypt/v3.jsx`)}/>
+    <Route path="/encrypt/2" component={require(`./pages/encrypt/v2.jsx`)}/>
+    <Route path="/encrypt/3" component={require(`./pages/encrypt/v3.jsx`)}/>
   </Router>)
 );
