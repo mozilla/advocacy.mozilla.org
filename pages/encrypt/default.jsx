6752352ec4b354a9a675b552d67d051a1e07f056
Ali Al Dallal <ali@alicoding.com>
2016-02-09 20:28:46 -0500

    Fix links for fb shares

--- pages/encrypt/default.jsx
+++ pages/encrypt/default.jsx
@@ -40,7 +40,7 @@ module.exports = React.createClass({
             <a target="_blank" href="mailto:?&subject=Sharing a video I thought you’d like. &body=I just watched a great video about online privacy and thought you would really like it. Check it out at https://mzl.la/encrypt. Hope you enjoy it as much as I did!" className="social-circle">
               <i className="fa fa-envelope"></i>
             </a>
-            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://advocacy.mozilla.org/encrypt/2/" className="social-circle">
+            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://advocacy.mozilla.org/encrypt/" className="social-circle">
               <i className="fa fa-facebook"></i>
             </a>
             <a target="_blank" href="https://twitter.com/intent/tweet?url=advocacy.mozilla.org/encrypt&text=I+just+watched+a+great+video+about+online+privacy+and+how+it+lets+%23youbeyou.+Check+it+out+at+mzl.la/encrypt" className="social-circle">
