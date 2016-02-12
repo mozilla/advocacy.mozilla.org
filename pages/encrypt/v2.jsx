5f73b9f1344db299ba201083c12d6e41a30cb49b
Alan Mooiman <amooiman@gmail.com>
2016-02-12 11:13:20 -0800

    Donate link in footer

--- pages/encrypt/v2.jsx
+++ pages/encrypt/v2.jsx
@@ -2,7 +2,6 @@ var React = require(`react`);
 var Footer = require(`../../components/footer.jsx`);
 var EncryptVideo = require(`../../components/encrypt-video.jsx`);
 var Signup = require(`../../components/encrypt-signup.jsx`);
-var Icon = require(`../../components/footer-icon.jsx`);
 var ShareThisNow = require(`../../components/encrypt-share-this-now`);
 var EncryptHeader = require(`../../components/encrypt-header`);
 var Modal = require(`../../components/encrypt-modal.jsx`);
