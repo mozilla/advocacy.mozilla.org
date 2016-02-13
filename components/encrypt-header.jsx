3df3f81ff6113c2255443e714728a12dd7298c5f
Alan Mooiman <amooiman@gmail.com>
2016-02-12 17:33:42 -0800

    Animation works!
    Stops on proper text

--- components/encrypt-header.jsx
+++ components/encrypt-header.jsx
@@ -1,6 +1,35 @@
 var React = require(`react`);
+var EncryptLogo = require('./encrypt-logo.jsx');
 
 module.exports = React.createClass({
+  componentDidMount() {
+    var positions = document.querySelectorAll(".Position");
+    var changableElements = document.querySelectorAll(".Position .Shapes > g, .Position .Chars > path");
+    var finalElements = document.querySelectorAll(".final");
+    Array.prototype.forEach.call(changableElements, (el)=>{el.style.display="none";});
+    var interval = setInterval(this.tick, 50, positions);
+
+    window.setTimeout(()=>{
+      this.stopChanging=true;
+      clearInterval(interval);
+      Array.prototype.forEach.call(changableElements, (el)=>{el.style.display="none";});
+      Array.prototype.forEach.call(finalElements, (el)=>{el.style.display="";});
+    }, 5000);
+
+  },
+  tick(ElGroup) {
+      const index = Math.floor(Math.random() * ElGroup.length);
+      var subGroup;
+      //Half the time change a shape, otherwise change a character
+      if(Math.random()>.5){
+        subGroup = ElGroup[index].querySelectorAll(".Shapes > g");
+      } else {
+        subGroup = ElGroup[index].querySelectorAll(".Chars > path");
+      }
+      Array.prototype.forEach.call(subGroup, (el)=>{el.style.display="none";});
+      var subGroupIndex = Math.floor(Math.random() * subGroup.length);
+      subGroup[subGroupIndex].style.display='';
+  },
   render: function() {
     return (
       <header className="header">
@@ -8,7 +37,7 @@ module.exports = React.createClass({
           <image className="logo" height="30" width="105" src="/assets/logo-mozilla.svg" alt="Mozilla"/>
           <div className="header-cta">Get Mozilla Advocacy Updates <button className="button" onClick={this.props.showModal}>Sign Up</button></div>
           <h1 className="encryptText">
-            <img src="/assets/Encrypt_Placeholder_Image.png" width="960" alt="Encrypt"/>
+            <EncryptLogo />
           </h1>
         </div>
       </header>
