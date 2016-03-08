d803161285c9cfe79530f9cf4faecca2cf28b1e4
Ali Al Dallal <ali@alicoding.com>
2016-03-08 15:43:07 -0500

    onenter

--- encrypt-main.jsx
+++ encrypt-main.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 /* eslint-disable no-unused-vars */
 import { Router, browserHistory, Route, IndexRedirect, Redirect } from 'react-router';
 
+function redirect(state, replace) {
+  if (state.location.query.video === '1') {
+    replace('/encrypt/direct/1');
+  }
+}
+
 module.exports = (
 	<Router history={browserHistory}>
 		<Route path="/encrypt">
@@ -12,7 +18,7 @@ module.exports = (
 			<Route path=":type/:video" component={require('./pages/encrypt/pageType.jsx')}/>
 			<Redirect from="direct" to="/encrypt/direct/2" />
 			<Redirect from="social" to="/encrypt/social/2" />
-			<Redirect from="2" to="/encrypt/direct/2" />
+			<Route path="2" onEnter={redirect} />
 			<Redirect from="3" to="/encrypt/hybrid/2" />
 			<Redirect from="*" to="/encrypt/" />
 		</Route>
