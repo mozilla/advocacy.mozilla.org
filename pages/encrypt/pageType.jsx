2f6649fc59e934bfc61e8e1d26a726c6f09db760
Alan Mooiman <amooiman@gmail.com>
2016-03-29 11:52:21 -0400

    Syntax, fix a CSS bug

--- pages/encrypt/pageType.jsx
+++ pages/encrypt/pageType.jsx
@@ -1,29 +1,29 @@
 var React = require('react');
 var pageTypes = {
-	'social': require(`./social.jsx`),
-	'direct': require(`./direct.jsx`),
-	'hybrid': require(`./hybrid.jsx`)
+  'social': require(`./social.jsx`),
+  'direct': require(`./direct.jsx`),
+  'hybrid': require(`./hybrid.jsx`)
 }
 
 var PageType = React.createClass({
-	propTypes: {
-		params: React.PropTypes.object.isRequired
-	},
-	render: function() {
-		var Page;
-        if(this.props.params.type == 'direct' && this.props.params.video == '4'){
-            Page = pageTypes.hybrid;
-        } else if(this.props.params && pageTypes[this.props.params.type]){
-			Page = pageTypes[this.props.params.type]
-		} else {
-			Page = pageTypes.social;
-		}
-		return (
-		  <div>
-			  <Page params={this.props.params} location={this.props.location}/>
-		  </div>
-		);
-	}
+  propTypes: {
+    params: React.PropTypes.object.isRequired
+  },
+  render: function() {
+    var Page;
+    if (this.props.params.type == 'direct' && this.props.params.video == '4') {
+      Page = pageTypes.hybrid;
+    } else if (this.props.params && pageTypes[this.props.params.type]) {
+      Page = pageTypes[this.props.params.type]
+    } else {
+      Page = pageTypes.social;
+    }
+    return (
+      <div>
+        <Page params={this.props.params} location={this.props.location}/>
+      </div>
+    );
+  }
 });
 
 module.exports = PageType;
