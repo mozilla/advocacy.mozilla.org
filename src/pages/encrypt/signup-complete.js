d3ee88b1ac1189e634aa510886c1e95a1dcbbfa8
ScottDowne <scott.downe@gmail.com>
2016-07-27 16:14:59 -0400

    update link in encrypt footer and make it better too

--- src/pages/encrypt/signup-complete.js
+++ src/pages/encrypt/signup-complete.js
@@ -1,8 +1,7 @@
 var React = require(`react`);
-var Footer = require(`../../components/footer.js`);
+var FooEncryptFooterter = require(`../../components/encrypt-footer.js`);
 var Signup = require(`../../components/encrypt-signup.js`);
 var classNames = require('classnames');
-var Icon = require(`../../components/footer-icon.js`);
 import { FormattedHTMLMessage } from 'react-intl';
 
 
@@ -28,9 +27,7 @@ module.exports = React.createClass({
             <p><i className="fa fa-arrow-right"></i><a className="exit-link" href={"/" + this.context.intl.locale + "/encrypt/2"}>{this.context.intl.formatMessage({id: 'take_me_to_encryption'})}</a></p>
           </div>
         </main>
-        <Footer>
-          <Icon href="https://medium.com/encryption-matters" src="/assets/footer-icon-medium.svg" title="Medium">{this.context.intl.formatMessage({id: 'join_the_convo'})}</Icon>
-        </Footer>
+        <EncryptFooter/>
       </div>
     );
   }
