a3899430fe68d52cf90ac63ea8d71a91268274de
Rebecca Lam <rebeccal@mozillafoundation.org>
2017-06-02 11:20:03 -0400

    Added the mobile header to the header component: hide on desktop display on mobile screens. Also made changes to signup-cta component to reflect mobile comps

--- src/components/safety/header.js
+++ src/components/safety/header.js
@@ -6,6 +6,8 @@ var Header = React.createClass({
       <div className="safety-header">
         <div className="container">
           <img src="/assets/safety/header.svg" className="header-image"/>
+          <div className="moz-logo">mozilla</div>
+          <img src="/assets/safety/mobile-header.svg" className="mobile-header-image"/>
         </div>
       </div>
     );
