c88cd014275748a385269421d14bf1decf2f993c
Rebecca Lam <rebeccal@mozillafoundation.org>
2017-06-06 11:37:12 -0400

    attempting to style the checkbox

--- src/components/safety/signup-form.js
+++ src/components/safety/signup-form.js
@@ -125,12 +125,12 @@ var SignupForm = React.createClass({
         </div>
         <input ref={(input) => { this.emailInput = input; }} value={this.state.emailInput} onChange={this.onEmailChange} className="email-input" placeholder="your email here" type="email"/>
         <ErrorMessage>{this.state.emailInputError}</ErrorMessage>
-        <label>
-          <input className="privacy-checkbox" type="checkbox" onChange={this.onPrivacyChange} value={this.state.privacyCheckboxChecked}/>
-          <span>
-            I'm ok with mozilla handling my info as explained in this <a href="">privacy notice</a>
-          </span>
-        </label>
+        <div>
+          <input className="privacy-checkbox" type="checkbox" id="c1" onChange={this.onPrivacyChange} value={this.state.privacyCheckboxChecked}/>
+          <label for="c1">
+          I'm ok with mozilla handling my info as explained in this <a href="">privacy notice</a>
+          </label>
+        </div>
         <ErrorMessage>{this.state.privacyCheckboxError}</ErrorMessage>
         <ErrorMessage>{this.state.submissionError}</ErrorMessage>
         <div>
