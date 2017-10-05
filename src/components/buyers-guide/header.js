b4cd269765a7e5d44752429123734349844df031
ScottDowne <scott.downe@gmail.com>
2017-10-05 11:56:17 -0400

    m

--- src/components/buyers-guide/header.js
+++ src/components/buyers-guide/header.js
@@ -5,10 +5,12 @@ var Header = React.createClass({
     return (
       <div className="buyers-guide-header-container">
         <div className="buyers-guide-header">
-          <button>
+          <button className="hamburger">
             <i className="fa fa-bars fa-2x"></i>
           </button>
-          <button>moz logo</button>
+          <button className="moz-logo">
+            <img src="/assets/moz-fav-bw-rgb-reverse.svg"/>
+          </button>
           <div className="social-buttons">
             <button className="social-button">
               <i className="fa fa-facebook fa-1x"></i>
