9a2ddb4785f903f950c28e1c99a4cf0e4b604d49
ScottDowne <scott.downe@gmail.com>
2017-10-17 16:06:40 -0400

    m

--- src/pages/buyers-guide/item.js
+++ src/pages/buyers-guide/item.js
@@ -12,7 +12,8 @@ var BuyersGuide = React.createClass({
       showModal: false,
       maybeLater: false,
       cancelTimeout: false,
-      signupSuccess: false
+      signupSuccess: false,
+      copyStatus: ""
     };
   },
   contextTypes: {
@@ -60,6 +61,70 @@ var BuyersGuide = React.createClass({
       cancelTimeout: true
     });
   },
+  copyLink: function() {
+    var textArea = document.createElement("textarea");
+
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a flash,
+    // so some of these are just precautions. However in IE the element
+    // is visible whilst the popup box asking the user for permission for
+    // the web page to copy to the clipboard.
+    //
+
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = window.location.href;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      this.setState({
+        copyStatus: "successful"
+      });
+      setTimeout(() => {
+        this.setState({
+          copyStatus: ""
+        });
+      }, 3000);
+    } catch (err) {
+      this.setState({
+        copyStatus: "unsuccessful"
+      });
+    }
+
+    document.body.removeChild(textArea);
+  },
   onSuccess: function() {
     this.setState({
       signupSuccess: true
@@ -71,7 +136,11 @@ var BuyersGuide = React.createClass({
     const items = guideData[category] || {};
     const item = items[itemName] || {};
     const locale = this.context.intl.locale;
-
+    const copyStatus = this.state.copyStatus;
+    var copyStatusClassName = "copy-status";
+    if (copyStatus === "successful") {
+      copyStatusClassName += " show";
+    }
     var modal = null;
     if (this.state.showModal) {
       if (this.state.signupSuccess) {
@@ -160,12 +229,15 @@ var BuyersGuide = React.createClass({
             category={category}
             item={itemName}
           />
+          <div className={copyStatusClassName}>
+            {this.context.intl.formatMessage({id: 'link_copied'})}
+          </div>
           <img src={item.img}/>
           <div className="clear">
             <span className="category-item-label">
               {item.label}
             </span>
-            <button className="copy-link">
+            <button className="copy-link" onClick={this.copyLink}>
               {this.context.intl.formatMessage({id: 'copy_link'})}
             </button>
           </div>
