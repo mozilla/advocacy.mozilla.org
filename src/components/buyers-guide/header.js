ea626cd38b95154cb643645a0bd0ef6e32559cf6
ScottDowne <scott.downe@gmail.com>
2017-10-17 12:25:54 -0400

    m

--- src/components/buyers-guide/header.js
+++ src/components/buyers-guide/header.js
@@ -16,36 +16,9 @@ var Header = React.createClass({
   },
   render: function() {
     const locale = this.context.intl.locale;
-    var menu = null;
+    var menuClassName = "menu-container playfair";
     if (this.state.menuOpen) {
-      menu = (
-        <div className="menu-container">
-          <a href={"/" + locale + "/buyers-guide/"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/toys"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/game-consoles"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/home-hubs"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/smart-home-accessories"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/gadgets-gizmos"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/category/health-excercise"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-          <a href={"/" + locale + "/buyers-guide/why-we-made"}>
-            {this.context.intl.formatMessage({id: 'todo'})}
-          </a>
-        </div>
-      );
+      menuClassName += " show";
     }
     return (
       <div className="buyers-guide-header-container">
@@ -68,7 +41,32 @@ var Header = React.createClass({
             </button>
           </div>
         </div>
-        {menu}
+        <div className={menuClassName}>
+          <a href={"/" + locale + "/buyers-guide/"}>
+            {this.context.intl.formatMessage({id: 'home_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/toys"}>
+            {this.context.intl.formatMessage({id: 'toys_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/game-consoles"}>
+            {this.context.intl.formatMessage({id: 'game_consoles_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/home-hubs"}>
+            {this.context.intl.formatMessage({id: 'home_hubs_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/smart-home-accessories"}>
+            {this.context.intl.formatMessage({id: 'smart_accessories_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/gadgets-gizmos"}>
+            {this.context.intl.formatMessage({id: 'gadgets_gizmos_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/category/health-excercise"}>
+            {this.context.intl.formatMessage({id: 'health_exercise_label'})}
+          </a>
+          <a href={"/" + locale + "/buyers-guide/why-we-made"}>
+            {this.context.intl.formatMessage({id: 'why_label'})}
+          </a>
+        </div>
       </div>
     );
   }
