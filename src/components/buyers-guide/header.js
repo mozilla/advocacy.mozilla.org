d17a8819197d5c9c55aceb77f81afdd60c44639d
ScottDowne <scott.downe@gmail.com>
2017-10-24 11:28:16 -0400

    m

--- src/components/buyers-guide/header.js
+++ src/components/buyers-guide/header.js
@@ -68,42 +68,42 @@ var Header = React.createClass({
           </div>
         </div>
         <div className={menuClassName}>
-          <div className="link-container">
+          <div className="link-container home-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/"}>
               {this.context.intl.formatMessage({id: 'home_label'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container toys-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/toys"}>
               {this.context.intl.formatMessage({id: 'cat_title_toys'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container game-consoles-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/game-consoles"}>
               {this.context.intl.formatMessage({id: 'cat_title_gameconsoles'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container home-hubs-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/home-hubs"}>
               {this.context.intl.formatMessage({id: 'cat_title_homehubs'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container smart-home-accessories-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/smart-home-accessories"}>
               {this.context.intl.formatMessage({id: 'cat_title_smarthomeaccessories'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container gadgets-gizmos-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/gadgets-gizmos"}>
               {this.context.intl.formatMessage({id: 'cat_title_gadgetsgizmos'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container health-excercise-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/health-excercise"}>
               {this.context.intl.formatMessage({id: 'cat_title_healthexcercise'})}
             </Link>
           </div>
-          <div className="link-container">
+          <div className="link-container why-link">
             <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/why-we-made"}>
               {this.context.intl.formatMessage({id: 'why_label'})}
             </Link>
