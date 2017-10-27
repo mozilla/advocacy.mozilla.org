e3686b7d083063a8952181282c1ff659b656127a
Théo Chevalier <theo.chevalier11@gmail.com>
2017-10-27 16:13:32 +0100

    Extract footer, comments intro, review categories

--- src/components/buyers-guide/footer.js
+++ src/components/buyers-guide/footer.js
@@ -12,22 +12,22 @@ var Footer = React.createClass({
         <div className="footer-contents">
           <div className="footer-links">
             <a href="https://www.mozilla.org/contact/" className="footer-link contact">
-              Contact Us
+              {this.context.intl.formatMessage({id: 'bg_footer_contact_us'})}
             </a>
             <a href={shareLink} className="footer-link share">
-              Share this page
+              {this.context.intl.formatMessage({id: 'bg_footer_share'})}
             </a>
             <a href="https://twitter.com/Mozilla" className="footer-link follow">
-              Follow
+              {this.context.intl.formatMessage({id: 'bg_footer_follow'})}
             </a>
             <a href="https://www.mozilla.org/about/legal/terms/mozilla/" className="footer-link legal">
-              Legal
+              {this.context.intl.formatMessage({id: 'bg_footer_legal'})}
             </a>
             <a href="https://www.mozilla.org/privacy/websites/" className="footer-link privacy">
-              Privacy
+              {this.context.intl.formatMessage({id: 'bg_footer_privacy'})}
             </a>
             <a href="https://donate.mozilla.org/" className="footer-link donate">
-              Donate
+              {this.context.intl.formatMessage({id: 'bg_footer_donate'})}
             </a>
           </div>
           <div className="org-info">
