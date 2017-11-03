93100890f69311c92e6ebb56c260fb9ebcf8544b
ScottDowne <scott.downe@gmail.com>
2017-11-03 14:42:37 -0400

    Fix scroll

--- src/components/buyers-guide/header.js
+++ src/components/buyers-guide/header.js
@@ -16,6 +16,10 @@ var Header = React.createClass({
       menuOpen: !this.state.menuOpen
     });
   },
+  onClick: function() {
+    window.scrollTo(0, 0);
+    this.toggleMenu();
+  },
   shareFbClick: function() {
     reactGA.event({
       category: "Social",
@@ -79,42 +83,42 @@ var Header = React.createClass({
         </div>
         <div className={menuClassName}>
           <div className="link-container home-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/"}>
               {this.context.intl.formatMessage({id: 'home_label'})}
             </Link>
           </div>
           <div className="link-container toys-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/toys"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/toys"}>
               {this.context.intl.formatMessage({id: 'cat_title_toys'})}
             </Link>
           </div>
           <div className="link-container game-consoles-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/gameconsoles"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gameconsoles"}>
               {this.context.intl.formatMessage({id: 'cat_title_gameconsoles'})}
             </Link>
           </div>
           <div className="link-container home-hubs-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/homehubs"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/homehubs"}>
               {this.context.intl.formatMessage({id: 'cat_title_homehubs'})}
             </Link>
           </div>
           <div className="link-container smart-home-accessories-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/smarthomeaccessories"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/smarthomeaccessories"}>
               {this.context.intl.formatMessage({id: 'cat_title_smarthomeaccessories'})}
             </Link>
           </div>
           <div className="link-container gadgets-gizmos-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/gadgetsgizmos"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gadgetsgizmos"}>
               {this.context.intl.formatMessage({id: 'cat_title_gadgetsgizmos'})}
             </Link>
           </div>
           <div className="link-container health-excercise-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/category/healthexcercise"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/healthexcercise"}>
               {this.context.intl.formatMessage({id: 'cat_title_healthexcercise'})}
             </Link>
           </div>
           <div className="link-container why-link">
-            <Link onClick={this.toggleMenu} to={"/" + locale + "/privacynotincluded/why-we-made"}>
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/why-we-made"}>
               {this.context.intl.formatMessage({id: 'wwmt_main_title'})}
             </Link>
           </div>
