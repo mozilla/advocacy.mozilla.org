336a0cf2ddad53000f89811d7b55068909fa1996
ScottDowne <scott.downe@gmail.com>
2017-11-06 16:32:37 -0500

    Adding back backgrounds.

--- src/components/buyers-guide/header.js
+++ src/components/buyers-guide/header.js
@@ -87,9 +87,6 @@ var Header = React.createClass({
           <div className="buyers-guide-header">
             {hamburger}
             <Link className="moz-logo" to={"/" + locale + "/privacynotincluded/"}>
-              <img src="/assets/moz-fav-bw-rgb-reverse.svg"/>
-            </Link>
-            <Link className="moz-logo" to={"/" + locale + "/privacynotincluded/"}>
             </Link>
             <Link className="moz-logo-2" to={"/" + locale + "/privacynotincluded/"}>
             </Link>
