05912c3d28cfbca9d401f7cb29490318c1310b97
ScottDowne <scott.downe@gmail.com>
2017-12-01 16:35:25 -0500

    og tags

--- src/components/net-neutrality/social.js
+++ src/components/net-neutrality/social.js
@@ -17,10 +17,10 @@ module.exports = React.createClass({
   render: function() {
     var appURL = "https://advocacy.mozilla.org";
     var locale = this.context.intl.locale;
-    var twitterShareURL = 'https://twitter.com/share?url=' + appURL +'/' + locale + '/net-neutrality/?text=' + encodeURIComponent("twitter share");
+    var twitterShareURL = 'https://twitter.com/share?url=' + appURL +'/' + locale + '/net-neutrality/&text=' + encodeURIComponent("Last week Federal Communications Commission Chairman Ajit Pai revealed the anti-net-neutrality proposal his agency is set to vote on December 14. Will you call Congress and ask them to stop the vote?");
     var facebookShareURL = 'https://www.facebook.com/sharer/sharer.php?u=' + appURL + '/' + locale + '/';
-    var emailSubject = "email subject";
-    var emailBody = "email body";
+    var emailSubject = "Join me in protecting net neutrality";
+    var emailBody = "Last week Federal Communications Commission Chairman Ajit Pai revealed the anti-net-neutrality proposal his agency is set to vote on December 14. Will you call Congress and ask them to stop the vote?";
     var emailShareURL = 'mailto:someone@example.com?subject='+ emailSubject +'&body='+ emailBody +'';
 
     return (
