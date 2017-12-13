5acc52635c8059cba96a0ddbe2721a325cc5703d
ScottDowne <scott.downe@gmail.com>
2017-12-13 18:45:26 -0500

    Proper sheet change

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ var routes = {
       "entry.1319525634": 'en-US'
     };
 
-    var url = "https://docs.google.com/a/mozillafoundation.org/forms/d/e/1FAIpQLSePp6pDW6lFErkI1sQGakk2pkP33NQm8A8Nwr9adlWCEmmfdA/formResponse";
+    var url = "https://docs.google.com/a/mozillafoundation.org/forms/d/e/1FAIpQLSePBZqnOdTzlN48mWtf1psXr_w6Ocb6ZZsJgrA4pRVoRM02ZA/formResponse";
 
     sheets(url, formData, 'https://advocacy.mozilla.org/net-neutrality/', transaction, transaction.signup, function(err, payload) {
       if (err) {
