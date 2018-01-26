f972caafc88be3083acd4c8e1b3d8456428afd38
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-01-25 16:35:22 -0800

    menu styling

--- src/components/buyers-guide/menu.js
+++ src/components/buyers-guide/menu.js
@@ -18,45 +18,53 @@ var Menu = React.createClass({
 
     return (
       <div className={menuClassName}>
-        <div className="link-container home-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/"}>
-            {this.context.intl.formatMessage({id: 'home_label'})}
-          </Link>
+        <div>
+          <button onClick={this.onClick} className="close-button"></button>
         </div>
-        <div className="link-container toys-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/toys"}>
-            {this.context.intl.formatMessage({id: 'cat_title_toys'})}
-          </Link>
-        </div>
-        <div className="link-container game-consoles-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gameconsoles"}>
-            {this.context.intl.formatMessage({id: 'cat_title_gameconsoles'})}
-          </Link>
-        </div>
-        <div className="link-container home-hubs-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/homehubs"}>
-            {this.context.intl.formatMessage({id: 'cat_title_homehubs'})}
-          </Link>
-        </div>
-        <div className="link-container smart-home-accessories-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/smarthomeaccessories"}>
-            {this.context.intl.formatMessage({id: 'cat_title_smarthomeaccessories'})}
-          </Link>
-        </div>
-        <div className="link-container gadgets-gizmos-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gadgetsgizmos"}>
-            {this.context.intl.formatMessage({id: 'cat_title_gadgetsgizmos'})}
-          </Link>
-        </div>
-        <div className="link-container health-excercise-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/healthexcercise"}>
-            {this.context.intl.formatMessage({id: 'cat_title_healthexcercise'})}
-          </Link>
-        </div>
-        <div className="link-container why-link">
-          <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/why-we-made"}>
-            {this.context.intl.formatMessage({id: 'wwmt_main_title'})}
-          </Link>
+
+        <div>
+          <div className="link-container home-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/"}>
+              {this.context.intl.formatMessage({id: 'home_label'})}
+            </Link>
+          </div>
+          <div className="link-container toys-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/toys"}>
+              {this.context.intl.formatMessage({id: 'cat_title_toys'})}
+            </Link>
+          </div>
+          <div className="link-container game-consoles-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gameconsoles"}>
+              {this.context.intl.formatMessage({id: 'cat_title_gameconsoles'})}
+            </Link>
+          </div>
+          <div className="link-container home-hubs-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/homehubs"}>
+              {this.context.intl.formatMessage({id: 'cat_title_homehubs'})}
+            </Link>
+          </div>
+          <div className="link-container smart-home-accessories-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/smarthomeaccessories"}>
+              {this.context.intl.formatMessage({id: 'cat_title_smarthomeaccessories'})}
+            </Link>
+          </div>
+          <div className="link-container gadgets-gizmos-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/gadgetsgizmos"}>
+              {this.context.intl.formatMessage({id: 'cat_title_gadgetsgizmos'})}
+            </Link>
+          </div>
+          <div className="link-container health-excercise-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/category/healthexcercise"}>
+              {this.context.intl.formatMessage({id: 'cat_title_healthexcercise'})}
+            </Link>
+          </div>
+          <div className="link-container why-link">
+            <Link onClick={this.onClick} to={"/" + locale + "/privacynotincluded/why-we-made"}>
+              {this.context.intl.formatMessage({id: 'wwmt_main_title'})}
+            </Link>
+          </div>
+
+          <a className="moz-logo" href="https://mozilla.org"></a>
         </div>
       </div>
     );
