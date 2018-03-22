aa46805d0e3006b01f068d6ea281ee950003b2ad
auremoser <auremoser@gmail.com>
2018-03-22 12:36:08 +0100

    add open application redirect to foundation site

--- src/pages/open-web-fellows/overview.js
+++ src/pages/open-web-fellows/overview.js
@@ -27,6 +27,9 @@ module.exports = React.createClass({
         <Header/>
         <HeroUnit image="/assets/overview.jpg">
           <h1>Open Web Fellows Program</h1>
+          <div>
+            <a href="https://foundation.mozilla.org/fellowships/apply/" className="button">Apply Now!</a>
+          </div>
         </HeroUnit>
         <FellowsHeader/>
         <div className="page">
@@ -128,6 +131,11 @@ module.exports = React.createClass({
             </div>
           </ContentContainer>
         </div>
+        <ApplyBanner 
+          title="Fellowship Application Open"
+          description="We are an inclusive program and seek Fellows that reflect the diversity of the communities we serve. We encourage unique contributors with varied backgrounds to apply; if you feel your perspective is under-represented in technology, we want to hear from you!"
+          cta = {{label:'Apply Now!', link:'https://foundation.mozilla.org/fellowships/apply/'}} />
+
         <Footer/>
       </div>
     );
